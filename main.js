f7d5c9faf637f1e16df307bcb80377d5684d822d
Brieanna  McDonald <bmcdonald1406@gmail.com>
2021-11-09 20:16:12 +0000

    Add: Calculation to convert epoch hourly time to hourly time

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ var temperaturemax = document.querySelector('.highs');
 var temperaturemin = document.querySelector('.Lows');
 var feelslike = document.querySelector ('.Feels_like');
 var btn = document.querySelector('.btn-local');
-
 //  hides the following classes until data is returned
 document.getElementById("hidden").style.display = "none";
 document.getElementById("hide").style.display = "none";
@@ -44,31 +43,52 @@ button.onclick = function() {
     let cityValue = data.name;
     city.innerHTML = cityValue;
  });
+ 
 // A different endpoint is used to return the data based on the lat and lon of the users input value
-let getGlobalForecast = function(lat, lon) {
-   fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely&units=metric&appid=5701594af7e6297f4750926ed730f1fd')
-    .then(response => response.json())
-    .then(Data => {
-   
-   //  provides variable names and the current data that should be returned.
-    var temperatureValue = Math.round(Data.current.temp);
-    var weatherValue = Data.current.weather[0].main;
-    var humidityValue = Data.current.humidity;
-    var pressureValue = Data.current.pressure;
-    var windspeedValue = Data.current.wind_speed;
-    var temperaturemaxValue = Math.round(Data.daily['0'].temp.max);
-    var temperatureminValue = Math.round(Data.daily['0'].temp.min);
-    var feelslikeValue = Math.round(Data.current.feels_like);
-    
-    // the returned data will then be displayed in the corrisponding html class.
-    temperature.innerHTML =temperatureValue + "°c";
-    weather.innerHTML =weatherValue;
-    humidty.innerHTML ="Humidity:"+ humidityValue + "°c";
-    pressure.innerHTML ="Pressure:"+ pressureValue + "hPa";
-    windspeed.innerHTML ="Wind sped:" + windspeedValue + "m/s";
-    temperaturemax.innerHTML ="H:" + temperaturemaxValue + "°c";
-    temperaturemin.innerHTML ="L:" +temperatureminValue+ "°c";
-    feelslike.innerHTML ="Feels like:"+ feelslikeValue + "°c";
-                 
-})};
+  let getGlobalForecast = function(lat, lon) {
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&exclude=minutely&units=metric&appid=5701594af7e6297f4750926ed730f1fd')
+ .then(response => response.json())
+ .then(Data => {
+
+//  provides variable names and the current data that should be returned.
+ var temperatureValue = Math.round(Data.current.temp);
+ var weatherValue = Data.current.weather[0].main;
+ var humidityValue = Data.current.humidity;
+ var pressureValue = Data.current.pressure;
+ var windspeedValue = Data.current.wind_speed;
+ var temperaturemaxValue = Math.round(Data.daily['0'].temp.max);
+ var temperatureminValue = Math.round(Data.daily['0'].temp.min);
+ var feelslikeValue = Math.round(Data.current.feels_like);
+ 
+ // the returned data will then be displayed in the corrisponding html class.
+ temperature.innerHTML =temperatureValue + "°c";
+ weather.innerHTML =weatherValue;
+ humidty.innerHTML ="Humidity:"+ humidityValue + "°c";
+ pressure.innerHTML ="Pressure:"+ pressureValue + "hPa";
+ windspeed.innerHTML ="Wind sped:" + windspeedValue + "m/s";
+ temperaturemax.innerHTML ="H:" + temperaturemaxValue + "°c";
+ temperaturemin.innerHTML ="L:" +temperatureminValue+ "°c";
+ feelslike.innerHTML ="Feels like:"+ feelslikeValue + "°c";
+              
+ // Hourly time in epoch time
+  let EpochTime1 = Data.hourly['1'].dt;
+  let EpochTime2 = Data.hourly['2'].dt;
+  let EpochTime3 = Data.hourly['3'].dt;
+  let EpochTime4 = Data.hourly['4'].dt;
+  let EpochTime5 = Data.hourly['5'].dt;
+            
+ // hourly time and date
+  let DateTime1 = new Date(EpochTime1 * 1000);
+  let DateTime2 = new Date(EpochTime2 * 1000);
+  let DateTime3 = new Date(EpochTime3 * 1000);
+  let DateTime4 = new Date(EpochTime4 * 1000);
+  let DateTime5 = new Date(EpochTime5 * 1000);
+ 
+ // hourly time
+  let Time1 = DateTime1.getHours();
+  let Time2 = DateTime2.getHours();
+  let Time3 = DateTime3.getHours();
+  let Time4 = DateTime4.getHours();
+  let Time5 = DateTime5.getHours();
+  })};
 };
