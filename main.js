99cad4a0333fa8780537eb4c4ae0b5a45b1f0e2f
Brieanna  McDonald <bmcdonald1406@gmail.com>
2021-11-09 19:25:05 +0000

    Add: HTML elements to declared variables

---
+++ main.js
@@ -0,0 +1,14 @@
+// selects the html elements for each variable
+var button = document.querySelector('.btn-global');
+var inputvalue = document.querySelector('.searchbar');
+var city = document.querySelector('.city');
+var temperature = document.querySelector('.Temp');
+var weather = document.querySelector('.weather');
+var humidty = document.querySelector('.Humidity');
+var pressure = document.querySelector('.pressure');
+var windspeed = document.querySelector('.wind');
+var temperaturemax = document.querySelector('.highs');
+var temperaturemin = document.querySelector('.Lows');
+var feelslike = document.querySelector ('.Feels_like');
+var btn = document.querySelector('.btn-local');
+
